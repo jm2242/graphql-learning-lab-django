9a7b617ed614e33b54b4626ada2abfa536be4128
Jonathan Mares <jm2242@cornell.edu>
2017-12-06 17:35:17 -0500

    upvotes to work

--- links/models.py
+++ links/models.py
@@ -6,3 +6,7 @@ from django.db import models
 class Link(models.Model):
     url = models.URLField()
     description = models.TextField(null=True, blank=True)
+
+
+class Vote(models.Model):
+    link = models.ForeignKey('links.Link', related_name='votes', on_delete=models.deletion.CASCADE)
--- links/schema.py
+++ links/schema.py
@@ -1,19 +1,29 @@
 import graphene
 from graphene_django import DjangoObjectType
 
-from links.models import Link
+from links.models import Link, Vote
 
 
 class LinkType(DjangoObjectType):
     class Meta:
         model = Link
 
+class VoteType(DjangoObjectType):
+    class Meta:
+        model = Vote
+
 
 class Query(graphene.ObjectType):
     links = graphene.List(LinkType)
 
+    votes = graphene.List(VoteType)
+
+
     def resolve_links(self, info, **kwargs):
         return Link.objects.all()
+
+    def resolve_votes(self, info, **kwargs):
+        return Vote.objects.all()
 
 
 class CreateLink(graphene.Mutation):
@@ -37,7 +47,24 @@ class CreateLink(graphene.Mutation):
             description=link.description,
         )
 
+class CreateVote(graphene.Mutation):
+    link = graphene.Field(LinkType)
+
+    class Arguments:
+        link_id = graphene.Int()
+
+    def mutate(self, info, link_id):
+        link = Link.objects.filter(id=link_id).first()
+        if not link:
+            raise Exception('Invalid Link!')
+
+        Vote.objects.create(
+            link=link,
+        )
+
+        return CreateVote(link=link)
 
 #4
 class Mutation(graphene.ObjectType):
     create_link = CreateLink.Field()
+    create_vote = CreateVote.Field()
